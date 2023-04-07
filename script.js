7437454edcee57c59e9d825bdf403e8556474a0f
Kutman7 <kutman.stel@gmail.com>
2023-04-07 16:47:47 +0200

    using classList.toggle to toggle menu

--- script.js
+++ script.js
@@ -19,31 +19,17 @@ function ParallaxEffect(e) {
 window.addEventListener("mousemove", ParallaxEffect)
 
 function toggle() {
-    document.querySelector(".mainToggleContainer").classList.add("toogleAnimation")
-    document.querySelector(".toggleContainer").classList.add("toogleAnimation1")
+    document.querySelector(".burgerNav").classList.toggle("menuAppear");
+    document.querySelector(".contactIcons").classList.toggle("contactMenuAppear")
+    document.querySelector(".xIcon").classList.toggle("xIconAppear")
+    document.querySelector(".mainToggleContainer").classList.toggle("toogleAnimation")
+    document.querySelector(".toggleContainer").classList.toggle("toogleAnimation1")
 
-    document.querySelector(".mainToggleContainer").classList.remove("unToogleAnimation")
-    document.querySelector(".toggleContainer").classList.remove("unToogleAnimation1")
-
-    document.querySelector(".burgerNav").classList.remove("menuDisappear");
-    document.querySelector(".burgerNav").classList.add("menuAppear");
-
-    document.querySelector(".contactIcons").classList.remove("contactMenuDisappear")
-    document.querySelector(".contactIcons").classList.add("contactMenuAppear")
-
-    // document.querySelector(".xIcon").classList.remove("xIconDisappear")
-    document.querySelector(".xIcon").classList.add("xIconAppear")
-
-    window.scrollTo(0, 0);
-    document.body.style.overflow = "hidden";
-}
-
-function unToggle() {
-    document.querySelector(".mainToggleContainer").classList.add("unToogleAnimation")
-    document.querySelector(".toggleContainer").classList.add("unToogleAnimation1")
-
-    document.querySelector(".burgerNav").classList.replace("menuAppear", "menuDisappear");
-    document.querySelector(".contactIcons").classList.replace("contactMenuAppear", "contactMenuDisappear"); 
-    document.querySelector(".xIcon").classList.remove("xIconAppear"); 
-    document.body.style.overflow = null;
+    if (document.querySelector(".xIcon").classList.contains("xIconAppear")) {
+        window.scrollTo(0, 0);
+        document.body.style.overflow = "hidden";
+    }
+    else {
+        document.body.style.overflow = null;
+    }
 }
