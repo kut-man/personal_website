d6dd589a9455c70e0d72cc5425eb1d2ea81a4c20
Kutman7 <kutman.stel@gmail.com>
2023-04-14 21:22:29 +0200

    Update navbar.jsx
    rendering navbar on client side because navbar is interactive and I should use useState

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -1,7 +1,25 @@
+"use client";
+
 import styles from './page.module.scss'
 import ContactIcons from '../contact_icons/contact_icons';
+import { useState } from 'react';
 
-export default function Navbar() {
+const Navbar = () => {
+
+    const [toggled, setToggled] = useState(false);
+
+    function toggle() {
+        document.querySelector(".toggleContainer>.contactIcons").classList.toggle("contactMenuAppear")
+    
+        if (document.querySelector(".xIcon").classList.contains("xIconAppear")) {
+            window.scrollTo(0, 0);
+            document.body.style.overflow = "hidden";
+        }
+        else {
+            document.body.style.overflow = null;
+        }
+    }
+    
     return (
         <nav className={styles.nav}>
 
@@ -23,18 +41,18 @@ export default function Navbar() {
                 </li>
             </ul>
 
-            <span onclick="toggle()">
+            <span onClick={() => setToggled(true)}>
                 <div style={{ width: 45 }} />
                 <div style={{ width: 35, transform: "translate(10px)" }} />
                 <div style={{ width: 25, transform: "translate(20px)" }} />
             </span>
 
-            <div className={styles.mainToggleContainer}></div>
-            <div className={styles.toggleContainer}>
+            <div className={`${styles.mainToggleContainer} ${toggled ? styles.toogleAnimation: ''}`}></div>
+            <div className={`${styles.toggleContainer} ${toggled ? styles.toogleAnimation1: ''}`}>
 
-                <div className={styles.xIcon}></div>
+                <div onClick={() => setToggled(false)} className={`${styles.xIcon} ${toggled ? styles.xIconAppear: ''}`}></div>
 
-                <ul className={styles.burgerNav}>
+                <ul className={`${styles.burgerNav} ${toggled ? styles.menuAppear: ''}`}>
                     <li>Home</li>
                     <li>Case Studies</li>
                     <li>Experiments</li>
@@ -48,3 +66,5 @@ export default function Navbar() {
         </nav>
     )
 }
+
+export default Navbar;
