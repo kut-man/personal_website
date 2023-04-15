b92531b4ee9232b3c9e257d352572ab6f176eb46
Kutman7 <kutman.stel@gmail.com>
2023-04-15 16:59:49 +0200

    refactoring

--- app/components/projects/projects.jsx
+++ app/components/projects/projects.jsx
@@ -17,19 +17,26 @@ const Projects = () => {
     }, [inView]);
 
     return (
-        <div ref={ref} className={styles.projectsContainer}>
-
+        <div className={styles.container}>
             <h6>CASE STUDIES</h6>
             <h4>Latest Works</h4>
+            <div ref={ref} className={styles.projectContainer}>
+                <div className={`${isInView ? styles.show : ''} ${styles.revealer}`}></div>
+                <div className={styles.project}>
 
-            <div className={styles.firstProjectContainer}>
-                <img src="http://riccardozanutta.com/img/cerasa-work.jpg" alt="Project Image"></img>
-                <h5>Cerasa Redesing</h5>
-                <p>Leader in bathroom furniture since 1983.</p>
-                <Button />
-                <div className={`${isInView ? styles.show : ''} ${styles.revealer}`} ></div>
+                    <h5>Cerasa Redesing</h5>
+                    <p>Leader in bathroom furniture since 1983.</p>
+
+                    <div className={styles.button}>
+                        <button>Case Study</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 476.213 476.213">
+                            <path
+                                d="M405.606 167.5l-21.212 21.213 34.393 34.393H0v30h418.787L384.394 287.5l21.212 21.213 70.607-70.607">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
