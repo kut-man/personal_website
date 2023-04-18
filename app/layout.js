106d1207d5f47c79c58a6b4291e8b9f812b330f1
Kutman7 <kutman.stel@gmail.com>
2023-04-19 00:33:48 +0200

    SEO

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,27 @@ export const metadata = {
   title: 'Kutman Eshenkulov',
   description: 'Personal website developed by Kutman Eshenkulov',
   keywords: ['Kutman', 'KUTMAN', "Kutman Eshenkulov", "Кутман Эшенкулов"],
-
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': 'auto',
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  og: {
+    title: "Kutman Eshenkulov",
+    description: "Personal website developed by Kutman Eshenkulov",
+    type: 'website',
+    url: 'https://kutman.lol',
+    image: 'https://user-images.githubusercontent.com/73386100/231491201-77f9d1d4-ead5-434f-9959-bb07b9bf0369.png',
+    site_name: "Kutman",
+  }
 };
 
 const spartan = League_Spartan({
