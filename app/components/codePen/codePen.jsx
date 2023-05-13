20ca78400febaef0a797535176497497b4dfa774
Kutman7 <kutman.stel@gmail.com>
2023-05-13 20:15:14 +0200

    styling experiments section

--- app/components/codePen/codePen.jsx
+++ app/components/codePen/codePen.jsx
@@ -4,13 +4,13 @@ export default function CodePen() {
     return (
         <a className={styles.link} href="http://codepen.io/rickzanutta/pen/yjaeB" target="_blank" rel="noopener" class="experiment">
             <div className={styles.container}>                
-                <div>
-                    <h4>- TITLE</h4>
+                <div className={styles.title}>
+                    <h5>TITLE</h5>
                     <p>CSS3 spin preloader + prelaod Page</p>
                 </div>
                 <div className={styles.number}>
-                    <h4>01</h4>
                     <div></div>
+                    <h4>01</h4>
                 </div> 
             </div>
             <div className={styles.circle}></div>
