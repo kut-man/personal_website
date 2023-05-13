b52ad082b54f9baa7e240cf1b23325e2fd71bd7d
Kutman7 <kutman.stel@gmail.com>
2023-05-14 00:07:23 +0200

    making experiments component mobile friendly

--- app/components/codePen/codePen.jsx
+++ app/components/codePen/codePen.jsx
@@ -3,15 +3,16 @@ import styles from './page.module.scss'
 export default function CodePen() {
     return (
         <a className={styles.link} href="http://codepen.io/rickzanutta/pen/yjaeB" target="_blank" rel="noopener" class="experiment">
-            <div className={styles.container}>                
+            <div className={styles.container}>
+                <h1>01</h1>
                 <div className={styles.title}>
                     <h5>TITLE</h5>
                     <p>CSS3 spin preloader + prelaod Page</p>
                 </div>
                 <div className={styles.number}>
-                    <div></div>
                     <h4>01</h4>
-                </div> 
+                    <div></div>
+                </div>
             </div>
             <div className={styles.circle}></div>
         </a>
